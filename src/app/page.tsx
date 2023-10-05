b30b9cfb5a9fbaf2f96373e9976903f49aacb229
Victor Hugo <grellheist@tutanota.com>
2023-10-04 21:57:12 -0300

    yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
         <div>
-            <div>BINGUS SHOP!!!!!!!!!!!!</div>
+            <h1>BINGUS SHOP!!!!!!!!!!!!</h1>
+            <p>hello how are you im under the water</p>
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.redd.it%2F42ah7pbr5it61.jpg%3Fauto%3Dwebp%26s%3D1a9c646d36f434e76c68881c9d4e3eb4cc0bdbe6&f=1&nofb=1&ipt=fd9d556de901937bc19d6a8b0d2d315608050475ae7b790f5770c9ec55288091&ipo=images" alt="" />
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.redd.it%2F42ah7pbr5it61.jpg%3Fauto%3Dwebp%26s%3D1a9c646d36f434e76c68881c9d4e3eb4cc0bdbe6&f=1&nofb=1&ipt=fd9d556de901937bc19d6a8b0d2d315608050475ae7b790f5770c9ec55288091&ipo=images" alt="" />
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.redd.it%2F42ah7pbr5it61.jpg%3Fauto%3Dwebp%26s%3D1a9c646d36f434e76c68881c9d4e3eb4cc0bdbe6&f=1&nofb=1&ipt=fd9d556de901937bc19d6a8b0d2d315608050475ae7b790f5770c9ec55288091&ipo=images" alt="" />
