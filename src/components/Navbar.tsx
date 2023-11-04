e307ba2377d67f09ee0e44ee422b518949e9933b
Victor Hugo <grellheist@tutanota.com>
2023-11-04 18:48:33 -0300

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,14 +11,14 @@ export const Navbar = () => {
 
     return (
         <>
-            <nav className='flex items-center flex-wrap bg-green-400 p-3 '>
+            <nav className='flex items-center flex-wrap bg-transparent p-3 '>
                 <Link href='/' className='inline-flex items-center p-2 mr-4 '>
                     <span className='text-xl text-white font-bold uppercase tracking-wide'>
-                        Bingus Shop
+                        Bingus!!!!!!!!!!!!
                     </span>
                 </Link>
                 <button
-                    className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+                    className=' inline-flex p-3 hover:bg-yellow-500 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
                     onClick={handleClick}
                 >
                     <svg
@@ -41,16 +41,16 @@ export const Navbar = () => {
                         }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
                 >
                     <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white '>
                             Home
                         </Link>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white'>
                             Services
                         </Link>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white'>
                             About us
                         </Link>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white'>
                             Contact us
                         </Link>
                     </div>
