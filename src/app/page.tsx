2c8d1f6b1a16b2bb811d68d969c610dadbb7e810
Victor Hugo <grellheist@tutanota.com>
2023-11-04 18:53:16 -0300

    better like this yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 export default function Home() {
     return (
-        <div className="flex flex-col items-center gap-12 p-12">
-            <h1 className="text-4xl font-bold">Bingus is the best</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-        </div>
+        <>
+            <section id="title" className="flex flex-col items-center gap-12 p-12">
+                <h1 className="text-4xl font-bold">Bingus is the best</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
+            </section>
+            <section id="services" className="flex flex-col items-center gap-12 p-12">
+
+            </section>
+            <section id="about" className="flex flex-col items-center gap-12 p-12">
+
+            </section>
+            <section id="contact" className="flex flex-col items-center gap-12 p-12">
+
+            </section>
+        </>
     )
 }
