a8b23e37b89a6cd9ac32335695bb346a7adc2b3c
Victor Hugo <grellheist@tutanota.com>
2023-11-06 17:13:51 -0300

    alright let's finish this already

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ export default function Home() {
         <>
             <section id="title" className="flex flex-col items-center gap-12 p-12">
                 <h1 className="text-4xl font-bold">Bingus is the best</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quasi veritatis aliquam nemo esse inventore iste nulla! Autem, vitae mollitia. Placeat dolorum molestias quasi sapiente necessitatibus asperiores corrupti quam enim?</p>
             </section>
             <section id="services" className="flex flex-col items-center gap-12 p-12">
 
