4fca0407f20481337152804c84dd9b1e928339ba
Victor Hugo <grellheist@tutanota.com>
2023-11-06 23:09:42 -0300

    fck i did it again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,15 @@ export default function Home() {
                 <h1 className="text-4xl font-bold">Bingus is the best</h1>
             </section>
             <section id="services" className="flex flex-col items-center gap-12 p-12">
+                <h1 className="text-4xl font-bold">Our services</h1>
 
             </section>
             <section id="about" className="flex flex-col items-center gap-12 p-12">
+                <h1 className="text-4xl font-bold">About bingus</h1>
 
             </section>
             <section id="contact" className="flex flex-col items-center gap-12 p-12">
+                <h1 className="text-4xl font-bold">Contact this rat</h1>
 
             </section>
         </>
