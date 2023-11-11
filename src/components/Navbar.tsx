7d41052f36a4b20ca5f832cd3c272d3ecfc91e73
Victor Hugo <grellheist@tutanota.com>
2023-11-11 13:39:01 -0300

    better colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
                     </span>
                 </Link>
                 <button
-                    className=' inline-flex p-3 hover:bg-yellow-500 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+                    className=' inline-flex p-3 hover:bg-[#FB8500] rounded lg:hidden text-white ml-auto hover:text-white outline-none'
                     onClick={handleClick}
                 >
                     <svg
@@ -41,16 +41,16 @@ export const Navbar = () => {
                         }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
                 >
                     <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white '>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#FB8500] hover:text-white '>
                             Home
                         </Link>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white'>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#FB8500] hover:text-white'>
                             Services
                         </Link>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white'>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#FB8500] hover:text-white'>
                             About us
                         </Link>
-                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-500 hover:text-white'>
+                        <Link href='/' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#FB8500] hover:text-white'>
                             Contact us
                         </Link>
                     </div>
